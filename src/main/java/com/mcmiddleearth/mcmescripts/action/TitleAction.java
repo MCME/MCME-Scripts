0ccc20b34742555cfef08002d44924867436f243
EriolEandur <Eriol_Eandur@gmx.de>
2022-11-17 18:06:34 +0100

    added placeholders *name* and *message* to TitleAction

--- src/main/java/com/mcmiddleearth/mcmescripts/action/TitleAction.java
+++ src/main/java/com/mcmiddleearth/mcmescripts/action/TitleAction.java
@@ -10,7 +10,17 @@ public class TitleAction extends SelectingAction<Player> {
     public TitleAction(Selector<Player> selector, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
         super(selector, (player, context) -> {
             DebugManager.verbose(Modules.Action.execute(TitleAction.class),"Title for player: "+player.getName() +" "+title);
-            player.sendTitle(title,subtitle,fadeIn,stay,fadeOut);
+            String finalTitle = title;
+            String finalSubtitle = subtitle;
+            if(context.getMessage()!=null) {
+                finalTitle = title.replace("*message*", context.getMessage());
+                finalSubtitle = subtitle.replace("*message*", context.getMessage());
+            }
+            if(context.getName()!=null) {
+                finalTitle = title.replace("*name*", context.getName());
+                finalSubtitle = subtitle.replace("*name*", context.getName());
+            }
+            player.sendTitle(finalTitle,finalSubtitle,fadeIn,stay,fadeOut);
         });
         //DebugManager.info(Modules.Action.create(this.getClass()),"Selector: "+selector.getSelector()+" Title: "+title);
         getDescriptor().indent()
